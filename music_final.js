9f71184057ffc3e77b3501236e4328acef770a16
smchardie1 <mchardie.stella@gmail.com>
2025-09-13 20:45:46 +1200

    Last update

--- music_final.js
+++ music_final.js
@@ -1,8 +1,9 @@
-// vocal, drum, bass, and other are volumes ranging from 0 to 100
+// preset colour palette
 let red = [235, 61, 68]
 let yellow = [246, 245, 181]
 let white = [255, 255, 255]  
 let black = [0]
+// assigning colours to a changing varible
 let colour1 = red;
 let colour2 = yellow;
 let colour3 = white;
@@ -16,68 +17,43 @@ let currentYellowSpikeSize = 270;
 
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   background(colour4)
-  textFont('Verdana'); // please use CSS safe fonts
-  if (counter >= 3315 && counter <= 3348) {
+  textFont('Verdana'); 
+  if (counter >= 3315 && counter <= 3348 || // start
+  counter >= 3417 && counter <= 3448 || 
+  counter >= 3521 && counter <= 3552 || 
+  counter >= 3625 && counter <= 3656 || 
+  counter >= 3725 && counter <= 3762 || 
+  counter >= 7961 && counter <= 8238 || // first middle
+  counter >= 8493 && counter <= 8767 || 
+  counter >= 12081 && counter <= 12355 || // second middle
+  counter >= 12617 && counter <= 12878 || 
+  counter >= 13183 && counter <= 13220 || // end
+  counter >= 13287 && counter <= 13324 || 
+  counter >= 13395 && counter <= 13434 || 
+  counter >= 13503 && counter <= 13536) {
   colour1 = black;
   colour2 = white;
   colour3 = yellow;
   colour4 = red;
   }
-  else if (counter >= 3349 && counter <= 3384) {
+  else if (counter >= 3349 && counter <= 3384 || // start
+  counter >= 3449 && counter <= 3484 ||
+  counter >= 3553 && counter <= 3588 || 
+  counter >= 3657 && counter <= 3693 || 
+  counter >= 3763 && counter <= 3796 || 
+  counter >= 8239 && counter <= 8492 || // first middle
+  counter >= 8768 && counter <= 9094 ||
+  counter >= 12356 && counter <= 12616 || // second middle
+  counter >= 12879 && counter <= 13194 ||
+  counter >= 13221 && counter <= 13258 || // end
+  counter >= 13325 && counter <= 13358 || 
+  counter >= 13435 && counter <= 13464 || 
+  counter >= 13537 && counter <= 13569) {
   colour1 = yellow;
   colour2 = red;
   colour3 = black;
   colour4 = white;
-  } 
-  else if (counter >= 3417 && counter <= 3448) {
-  colour1 = black;
-  colour2 = white;
-  colour3 = yellow;
-  colour4 = red;
-  }
-  else if (counter >= 3449 && counter <= 3484) {
-  colour1 = yellow;
-  colour2 = red;
-  colour3 = black;
-  colour4 = white;
-  } 
-  else if (counter >= 3521 && counter <= 3552) {
-  colour1 = black;
-  colour2 = white;
-  colour3 = yellow;
-  colour4 = red;
-  }
-  else if (counter >= 3553 && counter <= 3588) {
-  colour1 = yellow;
-  colour2 = red;
-  colour3 = black;
-  colour4 = white;
-  } 
-  else if (counter >= 3625 && counter <= 3656) {
-  colour1 = black;
-  colour2 = white;
-  colour3 = yellow;
-  colour4 = red;
-  }
-  else if (counter >= 3657 && counter <= 3693) {
-  colour1 = yellow;
-  colour2 = red;
-  colour3 = black;
-  colour4 = white;
-  }
-  else if (counter >= 3725 && counter <= 3762) {
-  colour1 = black;
-  colour2 = white;
-  colour3 = yellow;
-  colour4 = red;
-  }
-  else if (counter >= 3763 && counter <= 3796) {
-  colour1 = yellow;
-  colour2 = red;
-  colour3 = black;
-  colour4 = white;
-  } 
-  else {
+  } else {
     colour1 = red;
     colour2 = yellow;
     colour3 = white;
@@ -90,52 +66,55 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   record(vocal);
   console.log(counter);
 
+  //text and TAKE ME OUT layout
+  if (counter >= 5438 && counter <= 5526 || counter >= 5990 && counter <= 6086 || counter >= 7062 && counter <= 7167 || counter >= 7637 && counter <= 7729 || counter >= 10079 && counter <= 10159 || counter >= 10636 && counter <= 10715 || counter >= 11185 && counter <= 11293) {
+  textSize(120);
+  textAlign(CENTER);
+  textStyle(BOLD);
+  stroke(colour3);
+  strokeWeight(10);
+  fill(colour4);
   if (counter >= 5438 && counter <= 5526) {
-  textSize(120);
-  textAlign(CENTER);
-  textStyle(BOLD);
-  stroke(colour3);
-  strokeWeight(10);
-  fill(colour4);
-
-  if (counter >= 5438) {
-    text("TAKE", width / 2, 200);
+    if (counter >= 5438) text("TAKE", width / 2, 200);
+    if (counter >= 5476) text("ME", width / 2, 350);
+    if (counter >= 5494) text("OUT", width / 2, 500);
   }
-  if (counter >= 5476) {
-    text("ME", width / 2, 350);
+  else if (counter >= 5990 && counter <= 6086) {
+    if (counter >= 5990) text("TAKE", width / 2, 200);
+    if (counter >= 6027) text("ME", width / 2, 350);
+    if (counter >= 6052) text("OUT", width / 2, 500);
   }
-  if (counter >= 5494) {
-    text("OUT", width / 2, 500);
+  else if (counter >= 7062 && counter <= 7167) {
+    if (counter >= 7062) text("TAKE", width / 2, 200);
+    if (counter >= 7122) text("ME", width / 2, 350);
+    if (counter >= 7139) text("OUT", width / 2, 500);
   }
-} else {
-  // Regular lyrics
-  textSize(60);
-  lyrics(words);
-}
-  if (counter >= 5990 && counter <= 6086) {
-  textSize(120);
-  textAlign(CENTER);
-  textStyle(BOLD);
-  stroke(colour3);
-  strokeWeight(10);
-  fill(colour4);
-
-  if (counter >= 5990) {
-    text("TAKE", width / 2, 200);
+  else if (counter >= 7637 && counter <= 7729) {
+    if (counter >= 7637) text("TAKE", width / 2, 200);
+    if (counter >= 7670) text("ME", width / 2, 350);
+    if (counter >= 7686) text("OUT", width / 2, 500);
   }
-  if (counter >= 6027) {
-    text("ME", width / 2, 350);
+  else if (counter >= 10079 && counter <= 10159) {
+    if (counter >= 10079) text("TAKE", width / 2, 200);
+    if (counter >= 10116) text("ME", width / 2, 350);
+    if (counter >= 10126) text("OUT", width / 2, 500);
   }
-  if (counter >= 6052) {
-    text("OUT", width / 2, 500);
+  else if (counter >= 10636 && counter <= 10715) {
+    if (counter >= 10636) text("TAKE", width / 2, 200);
+    if (counter >= 10666) text("ME", width / 2, 350);
+    if (counter >= 10682) text("OUT", width / 2, 500);
+  }
+  else if (counter >= 11185 && counter <= 11293) {
+    if (counter >= 11185) text("TAKE", width / 2, 200);
+    if (counter >= 11221) text("ME", width / 2, 350);
+    if (counter >= 11235) text("OUT", width / 2, 500);
   }
 } else {
   // Regular lyrics
   textSize(60);
   lyrics(words);
+  }
 }
-}
-
 
 function recordShine(angle,size) {
   fill(colour3)
@@ -144,9 +123,9 @@ function recordShine(angle,size) {
   arc(0, 0, size, size, angle-85, angle-75);
 }
 function record(vocal) {
-  let targetSize = map(vocal, 20, 100, 250, 400);
-  currentRecordSize = lerp(currentRecordSize, targetSize, 0.3)
-  let recordStroke = map(currentRecordSize, 200, 380, 4, 16)
+  let targetSize = map(vocal, 20, 100, 250, 400); 
+  currentRecordSize = lerp(currentRecordSize, targetSize, 0.3) // controlling the smoothness of the transition
+  let recordStroke = map(currentRecordSize, 200, 380, 4, 16) // making the records stroke change in relation to the records size
   push();
   translate(width/2, height/2);
   rotate(recordSpin);
